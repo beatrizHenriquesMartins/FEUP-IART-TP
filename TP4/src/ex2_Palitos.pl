12c212995a4bf07b6dcfb0ea74df55e1915ac400
Beatriz de Henriques Martins <up201502858@fe.up.pt>
2018-03-16 12:57:42 +0000

    exercicio alinea b

--- TP4/src/ex2_Palitos.pl
+++ TP4/src/ex2_Palitos.pl
@@ -1,5 +1,6 @@
 :-use_module(library(lists)).
 
+% alinea a
 estado_inicial((10, max)).
 
 estado_final((0, max), 1).
@@ -72,8 +73,65 @@ min_value([E1|Es], MV, ME):-
             ME = E2
         ).
 
+% alinea b
+minimax(E, _, _,Valor, _):-
+        estado_final(E, Valor).
+
+minimax(E, _, 0, Valor, _):-
+        avalia(E, Valor).
+
+% procura o maior
+minimax(E, max, P, Valor, Jogador):-
+        findall(E2, sucessor(E, max, E2), LS),
+        P1 is P - 1,
+        max_value(LS, P1, Valor, Jogador).
+
+% procura o menor
+minimax(E, min, P, Valor, Jogador):-
+        findall(E2, sucessor(E, min, E2), LS),
+        P1 is P - 1,
+        min_value(LS, P1, Valor, Jogador).
+
+max_value([E], P, Valor, E):-
+        minimax(E, min, P, Valor, _).
+max_value([E1|Es], P, MV, ME):-
+        minimax(E1, min, P, V1, _),
+        max_value(Es, P, V2, E2),
+        (
+            % 1º estado
+            V1 > V2,
+            !,
+            MV = V1, % como não é uma expressão então não é necessário usar o is
+            ME = E1;
+            % 2º estado
+            MV = V2,
+            ME = E2
+        ).
+
+min_value([E], P, Valor, E):-
+        minimax(E, max, P, Valor, _).
+min_value([E1|Es], P, MV, ME):-
+        minimax(E1, max, P, V1, _),
+        min_value(Es, P, V2, E2),
+        (
+            % 1º estado
+            V1 < V2,
+            !,
+            MV = V1, % como não é uma expressão então não é necessário usar o is
+            ME = E1;
+            % 2º estado
+            MV = V2,
+            ME = E2
+        ).
+
 avalia((N, max), V):-
         1 is N mod 4,
         !,
         V = 0;
         V = 1.
+
+
+/*
+Como testar
+minimax((10, max), max, V, J).
+*/
